5ffe6390bbaf45b1cedfff753834b3c78e6eeeec
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-23 19:30:24 +0200

    Update categories_image_resize.php

--- new_files/lang/german/modules/system/categories_image_resize.php
+++ new_files/lang/german/modules/system/categories_image_resize.php
@@ -2,6 +2,8 @@
 $rthPrefix = 'MODULE_' . 'CATEGORIES_IMAGE_RESIZE_';
 
 define($rthPrefix . 'TITLE', 'Kategoriebild Größe automatisch anpassen © by <a href="https://bert-funke.de" target="_blank" style="color: #4DCDFF; font-weight: bold;">Bert Funke</a>');
-define($rthPrefix . 'LONG_DESCRIPTION', 'Mit dem Modul "Kategoriebild Größe automatisch anpassen" stehen Funktionen zur Verfügung, um Kategoriebilder zu vereinheitlichen.<br />Die benötigten Klassenerweiterungsmodule werden automatisch mit installiert!
+define($rthPrefix . 'LONG_DESCRIPTION', 'Mit dem Modul "Kategoriebild Größe automatisch anpassen" stehen Funktionen zur Verfügung, um Kategoriebilder zu vereinheitlichen.<br />Die benötigten Klassenerweiterungsmodule werden automatisch mit installiert!');
 define($rthPrefix . 'STATUS_TITLE', 'Kategoriebild Größe automatisch anpassen Modul aktivieren?');
 define($rthPrefix . 'STATUS_DESC', '');
+define($rthPrefix . 'REMOVE', '<b>Bitte beachten:</b> Neue Kategoriebilder müssen standardmäßig manuell angepasst werden!');
+define($rthPrefix . 'STATUS_INFO', 'Sobald das Modul deaktiviert ist,  müssen standardmäßig neue Kategoriebilder manuell angepasst werden!');
