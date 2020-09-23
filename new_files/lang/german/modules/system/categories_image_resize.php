b5611c4da369e9ffdaa65b8e9b39de9c00b10de8
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-23 19:27:15 +0200

    Create categories_image_resize.php

---
+++ new_files/lang/german/modules/system/categories_image_resize.php
@@ -0,0 +1,7 @@
+<?php
+$rthPrefix = 'MODULE_' . 'CATEGORIES_IMAGE_RESIZE_';
+
+define($rthPrefix . 'TITLE', 'Kategoriebild Größe automatisch anpassen © by <a href="https://bert-funke.de" target="_blank" style="color: #4DCDFF; font-weight: bold;">Bert Funke</a>');
+define($rthPrefix . 'LONG_DESCRIPTION', 'Mit dem Modul "Kategoriebild Größe automatisch anpassen" stehen Funktionen zur Verfügung, um Kategoriebilder zu vereinheitlichen.<br />Die benötigten Klassenerweiterungsmodule werden automatisch mit installiert!
+define($rthPrefix . 'STATUS_TITLE', 'Kategoriebild Größe automatisch anpassen Modul aktivieren?');
+define($rthPrefix . 'STATUS_DESC', '');
