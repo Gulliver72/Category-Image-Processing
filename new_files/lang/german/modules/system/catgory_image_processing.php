a420578ea34974320da864a93bcb21f557b1cda0
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-23 19:20:05 +0200

    Update and rename gul_catgory_image_processing.php to catgory_image_processing.php

--- new_files/lang/german/modules/system/catgory_image_processing.php
+++ new_files/lang/german/modules/system/catgory_image_processing.php
@@ -1,5 +1,5 @@
 <?php
-$rthPrefix = 'MODULE_' . 'GUL_CATEGORY_IMAGE_PROCESSING_';
+$rthPrefix = 'MODULE_' . 'CATEGORY_IMAGE_PROCESSING_';
 
 define($rthPrefix . 'TITLE', 'Image-Processing Kategoriebilder © by <a href="https://bert-funke.de" target="_blank" style="color: #4DCDFF; font-weight: bold;">Bert Funke</a>');
 define($rthPrefix . 'LONG_DESCRIPTION', 'Mit diesem Modul können bereits bestehende Kategoriebilder automatisch bearbeitet werden. Dabei erfolgt eine Bearbeitung analog der Produktbilder. Die Größen für die info_images und thumbnail_images können unter Konfiguration/Bild Optionen eingestellt werden. Auch Mergen wird unterstützt.');
