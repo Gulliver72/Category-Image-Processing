1b62e681af46e409776088620b9af418af1d3321
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-25 19:22:32 +0200

    Create categories_image.php

---
+++ new_files/admin/includes/extra/paths/categories_image.php
@@ -0,0 +1,15 @@
+<?php
+/* ------------------------------------------------------------
+	Module "Kategoriebild Größe automatisch anpassen" made by Gulliver72
+
+	inspired by MODUL Kategoriebilder Größe automatisch anpassen von web28 und astaller
+
+	modified eCommerce Shopsoftware
+	https://www.modified-shop.org
+
+	Released under the GNU General Public License
+-------------------------------------------------------------- */
+
+  defined('DIR_FS_CATALOG_CATEGORIES_IMAGES') or define('DIR_FS_CATALOG_CATEGORIES_IMAGES', DIR_FS_CATALOG_IMAGES .'categories/');
+  defined('DIR_FS_CATALOG_CATEGORIES_THUMBNAIL_IMAGES') or define('DIR_FS_CATALOG_CATEGORIES_THUMBNAIL_IMAGES', DIR_FS_CATALOG_CATEGORIES_IMAGES . 'thumbnail_images/');
+  defined('DIR_FS_CATALOG_CATEGORIES_INFO_IMAGES') or define('DIR_FS_CATALOG_CATEGORIES_INFO_IMAGES', DIR_FS_CATALOG_CATEGORIES_IMAGES .'info_images/');
