71835b98d770548fabee0efa30937fffd1d68cf2
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-28 14:53:19 +0200

    Create configuration_4.php

---
+++ new_files/lang/german/extra/admin/configuration_4.php
@@ -0,0 +1,13 @@
+<?php
+defined('CATEGORIES_IMAGE_HEIGHT_TITLE') OR define('CATEGORIES_IMAGE_HEIGHT_TITLE', 'Höhe der Kategorie Bilder');
+defined('CATEGORIES_IMAGE_HEIGHT_DESC') OR define('CATEGORIES_IMAGE_HEIGHT_DESC', 'Maximale Höhe der Kategorie Bilder in Pixeln. (Standard: 300)');
+defined('CATEGORIES_IMAGE_WIDTH_TITLE') OR define('CATEGORIES_IMAGE_WIDTH_TITLE', 'Breite der Kategorie Bilder');
+defined('CATEGORIES_IMAGE_WIDTH_DESC') OR define('CATEGORIES_IMAGE_WIDTH_DESC', 'Maximale Breite der Kategorie Bilder in Pixeln. (Standard: 300).');
+defined('CATEGORIES_IMAGE_MERGE_TITLE') OR define('CATEGORIES_IMAGE_MERGE_TITLE', 'Kategorie Bilder:Merge');
+defined('CATEGORIES_IMAGE_MERGE_DESC') OR define('CATEGORIES_IMAGE_MERGE_DESC', 'Kategorie-Bilder:Merge<br /><br />Default Wert: (overlay.gif,10,-50,60,FF0000)<br /><br />overlay merge image<br />Verwendung:<br />(merge image,x start [neg = from right],y start [neg = from base],opacity, transparent colour on merge image)');
+defined('CATEGORIES_IMAGE_THUMBNAIL_HEIGHT_TITLE') OR define('CATEGORIES_IMAGE_THUMBNAIL_HEIGHT_TITLE', 'Höhe der Kategorie Thumbnails');
+defined('CATEGORIES_IMAGE_THUMBNAIL_HEIGHT_DESC') OR define('CATEGORIES_IMAGE_THUMBNAIL_HEIGHT_DESC', 'Maximale Höhe der Kategorie Thumbnails in Pixeln. (Standard: 150)');
+defined('CATEGORIES_IMAGE_THUMBNAIL_WIDTH_TITLE') OR define('CATEGORIES_IMAGE_THUMBNAIL_WIDTH_TITLE', 'Breite der Kategorie Thumbnails');
+defined('CATEGORIES_IMAGE_THUMBNAIL_WIDTH_DESC') OR define('CATEGORIES_IMAGE_THUMBNAIL_WIDTH_DESC', 'Maximale Breite der Kategorie Thumbnails in Pixeln. (Standard: 150).');
+defined('CATEGORIES_IMAGE_THUMBNAIL_MERGE_TITLE') OR define('CATEGORIES_IMAGE_THUMBNAIL_MERGE_TITLE', 'Kategorie Thumbnails:Merge');
+defined('CATEGORIES_IMAGE_THUMBNAIL_MERGE_DESC') OR define('CATEGORIES_IMAGE_THUMBNAIL_MERGE_DESC', 'Kategorie Thumbnails:Merge<br /><br />Default Wert: (overlay.gif,10,-50,60,FF0000)<br /><br />overlay merge image<br />Verwendung:<br />(merge image,x start [neg = from right],y start [neg = from base],opacity, transparent colour on merge image)');
