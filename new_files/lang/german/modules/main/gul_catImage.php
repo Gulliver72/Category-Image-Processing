7f2dc0d355614a2d38d163e962ecf4c81c91aaa0
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-28 15:26:33 +0200

    Create gul_catImage.php

---
+++ new_files/lang/german/modules/main/gul_catImage.php
@@ -0,0 +1,2 @@
+  defined('MODULE_MAIN_CATIMAGE_TITLE') OR define('MODULE_MAIN_CATIMAGE_TITLE', 'Pfad das Kategoriebildes anpassen');
+  defined('MODULE_MAIN_CATIMAGE_DESCRIPTION') OR define('MODULE_MAIN_CATIMAGE_DESCRIPTION', 'Der Pfad des Kategoriebildes wird auf die bearbeiteten Bilder in /images/categories/info_images/ geändert, wenn das Bild dort vorhanden ist.');
