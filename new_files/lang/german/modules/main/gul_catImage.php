a25a0cc827b2d9eba6c28bb4cc8e9f1d7e4f011d
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-29 13:05:15 +0200

    Update gul_catImage.php

--- new_files/lang/german/modules/main/gul_catImage.php
+++ new_files/lang/german/modules/main/gul_catImage.php
@@ -1,2 +1,5 @@
-  defined('MODULE_MAIN_CATIMAGE_TITLE') OR define('MODULE_MAIN_CATIMAGE_TITLE', 'Pfad das Kategoriebildes anpassen');
-  defined('MODULE_MAIN_CATIMAGE_DESCRIPTION') OR define('MODULE_MAIN_CATIMAGE_DESCRIPTION', 'Der Pfad des Kategoriebildes wird auf die bearbeiteten Bilder in /images/categories/info_images/ geändert, wenn das Bild dort vorhanden ist.');
+<?php
+$rthPrefix = 'MODULE_MAIN' . 'GUL_CATIMAGE_';
+
+defined("$rthPrefix . 'TITLE'") OR define($rthPrefix . 'TITLE', 'Pfad das Kategoriebildes anpassen');
+defined("$rthPrefix . 'DESCRIPTION'") OR define($rthPrefix . 'DESCRIPTION', 'Der Pfad des Kategoriebildes wird auf die bearbeiteten Bilder in /images/categories/info_images/ geändert, wenn das Bild dort vorhanden ist.');
