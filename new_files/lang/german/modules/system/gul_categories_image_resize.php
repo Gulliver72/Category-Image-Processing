eb00d993900b415afa5bc89d842d81fbc36cbfcc
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-29 12:50:35 +0200

    Update and rename categories_image_resize.php to gul_categories_image_resize.php

--- new_files/lang/german/modules/system/gul_categories_image_resize.php
+++ new_files/lang/german/modules/system/gul_categories_image_resize.php
@@ -1,5 +1,5 @@
 <?php
-$rthPrefix = 'MODULE_' . 'CATEGORIES_IMAGE_RESIZE_';
+$rthPrefix = 'MODULE_' . 'GUL_CATEGORIES_IMAGE_RESIZE_';
 
 define($rthPrefix . 'TITLE', 'Kategoriebild Größe automatisch anpassen © by <a href="https://bert-funke.de" target="_blank" style="color: #4DCDFF; font-weight: bold;">Bert Funke</a>');
 define($rthPrefix . 'LONG_DESCRIPTION', 'Mit dem Modul "Kategoriebild Größe automatisch anpassen" stehen Funktionen zur Verfügung, um Kategoriebilder zu vereinheitlichen.<br />Die benötigten Klassenerweiterungsmodule werden automatisch mit installiert!');
