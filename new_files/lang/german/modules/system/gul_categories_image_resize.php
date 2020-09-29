94516fe7cd62c204438f11fd4f8621571a8b57e8
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-09-29 13:16:43 +0200

    Update gul_categories_image_resize.php

--- new_files/lang/german/modules/system/gul_categories_image_resize.php
+++ new_files/lang/german/modules/system/gul_categories_image_resize.php
@@ -1,9 +1,9 @@
 <?php
 $rthPrefix = 'MODULE_' . 'GUL_CATEGORIES_IMAGE_RESIZE_';
 
-define($rthPrefix . 'TITLE', 'Kategoriebild Größe automatisch anpassen © by <a href="https://bert-funke.de" target="_blank" style="color: #4DCDFF; font-weight: bold;">Bert Funke</a>');
-define($rthPrefix . 'LONG_DESCRIPTION', 'Mit dem Modul "Kategoriebild Größe automatisch anpassen" stehen Funktionen zur Verfügung, um Kategoriebilder zu vereinheitlichen.<br />Die benötigten Klassenerweiterungsmodule werden automatisch mit installiert!');
-define($rthPrefix . 'STATUS_TITLE', 'Kategoriebild Größe automatisch anpassen Modul aktivieren?');
-define($rthPrefix . 'STATUS_DESC', '');
-define($rthPrefix . 'REMOVE', '<b>Bitte beachten:</b> Neue Kategoriebilder müssen standardmäßig manuell angepasst werden!');
-define($rthPrefix . 'STATUS_INFO', 'Sobald das Modul deaktiviert ist,  müssen standardmäßig neue Kategoriebilder manuell angepasst werden!');
+defined("$rthPrefix . 'TITLE'") OR define($rthPrefix . 'TITLE', 'Kategoriebild Größe automatisch anpassen © by <a href="https://bert-funke.de" target="_blank" style="color: #4DCDFF; font-weight: bold;">Bert Funke</a>');
+defined("$rthPrefix . 'LONG_DESCRIPTION'") OR define($rthPrefix . 'LONG_DESCRIPTION', 'Mit dem Modul "Kategoriebild Größe automatisch anpassen" stehen Funktionen zur Verfügung, um Kategoriebilder zu vereinheitlichen.<br />Die benötigten Klassenerweiterungsmodule werden automatisch mit installiert!');
+defined("$rthPrefix . 'STATUS_TITLE'") OR define($rthPrefix . 'STATUS_TITLE', 'Kategoriebild Größe automatisch anpassen Modul aktivieren?');
+defined("$rthPrefix . 'STATUS_DESC'") OR define($rthPrefix . 'STATUS_DESC', '');
+defined("$rthPrefix . 'REMOVE'") OR define($rthPrefix . 'REMOVE', '<b>Bitte beachten:</b> Neue Kategoriebilder müssen standardmäßig manuell angepasst werden!');
+defined("$rthPrefix . 'STATUS_INFO'") OR define($rthPrefix . 'STATUS_INFO', 'Sobald das Modul deaktiviert ist,  müssen standardmäßig neue Kategoriebilder manuell angepasst werden!');
